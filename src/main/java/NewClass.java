ba18db651c081ba0f7e5c2d70f11772657dac5ef
Sukiisss <167664811+Sukiisss@users.noreply.github.com>
2024-06-11 03:38:48 -0400

    Update NewClass.java

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -26,22 +26,41 @@ public class Mastermind {
 
 
 public class Mastermind {
-
-    String[] colours = {"Red", "Orange", "Yellow", "Green", "Blue"};
-    String[] secretCode = new String[4];
+    public static void main(String[] args) {
+        String[] colours = {"Red", "Orange", "Yellow", "Green", "Blue"};
 
     Scanner scan = new Scanner(System.in);
+    System.out.println("Enter your guess (four colours separated by spaces):");
+    String guess = scan.nextLine();
 
-    public Mastermind() {
-        generateSecretCode();
-        playGame();
+    String colour1 = guess.substring(0, guess.indexOf(" "));
+    guess = guess.substring(guessInput.indextOf (" ") + 1);
+    String colour2 = guess.substring(0, guess.indexOf(" "));
+    guess = guess.substring(guess.indexOf(" ") + 1);
+    String colour3 = guess.substring(0, guess.indexOf(" "));
+    String colour4 = guess.substring(guess.indexOf(" ") + 1);
+
+    int correctColours = 0;
+    int correctPositions = 0;
+
+    String[] guessColours = {colour1, colour2, colour3, colour4};
+
+    for (int i = 0; i < guessColours.length; i++) {
+        String colour = guessColours[i];
+        for (int j = 0; j < colours.length; j++) {
+            if (colours[j].equals(colour)) {
+                correctColour++;
+                if (i == j) {
+                    correctPositions++;
+                }
+                break;
+            }
+        }
+    }
+
+    System.out.println("Correct colours : " + correctColours);
+    System.out.println("Correct positions: " + correctPositions);
     }
 }
 
-public void generateSecretCode() {
-    Random random = new Random();
-    for (int i = 0; i < secretCode.length; i++) {
-        int randomLine = random.nextInt(colours.length);
-        secretCode[i] = colours[randomIndext];
-    }
-}
+
