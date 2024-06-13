47de04ec570b3b577bc4e63ca5e02df51592d8d1
Sukiisss <167664811+Sukiisss@users.noreply.github.com>
2024-06-13 09:53:55 -0400

    Update NewClass.java

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -17,41 +17,24 @@ public class Mastermind {
         String[] colours = {"Red", "Orange", "Yellow", "Green", "Blue"};
 
         String[] secretCode = new String[4];
-        for (int i = 0; i < 4; i++) {
-            secretCode[i] = colours[random.nextInt(colours.length)];
+        
+        for (int i = 0; i < secretCode.length; i++) {
+            int randomColours = random.nextInt(colours.length);
+            secretCode[i] = colours[randomColours];
         }
+    }
         
     System.out.println("Enter your guess (four colours separated by spaces):");
-    String guess = scan.nextLine();
+    String[] guess = new String[4];
 
-    String colour1 = guess.substring(0, guess.indexOf(" "));
-    guess = guess.substring(guessInput.indextOf (" ") + 1);
-    String colour2 = guess.substring(0, guess.indexOf(" "));
-    guess = guess.substring(guess.indexOf(" ") + 1);
-    String colour3 = guess.substring(0, guess.indexOf(" "));
-    String colour4 = guess.substring(guess.indexOf(" ") + 1);
-
-    int correctColours = 0;
+    for (int i = 0; i < guess.length; i++) {
+        guess[i] = scan.nextLine();
+    }
+        
     int correctPositions = 0;
-
-    String[] guessColours = {colour1, colour2, colour3, colour4};
-
-    for (int i = 0; i < guessColours.length; i++) {
-        String colour = guessColours[i];
-        for (int j = 0; j < colours.length; j++) {
-            if (colours[j].equals(colour)) {
-                correctColour++;
-                if (i == j) {
-                    correctPositions++;
-                }
-                break;
-            }
+    for (int i = 0; i < secretCode.length; i++) {
+        if (guess[i].equals(secretCode[i])) {
+            correctPositions++;
         }
     }
-
-    System.out.println("Correct colours : " + correctColours);
-    System.out.println("Correct positions: " + correctPositions);
-    }
 }
-
-
